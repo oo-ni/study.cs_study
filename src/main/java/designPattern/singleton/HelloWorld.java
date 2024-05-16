5a45a85b661a75b01811e1739f4de15e56382c72
GeonWoo <69147082+Y-gw@users.noreply.github.com>
2024-05-16 17:11:50 +0900

    design pattern 추가

---
+++ src/main/java/designPattern/singleton/HelloWorld.java
@@ -0,0 +1,24 @@
+package designPattern.singleton;
+
+class Singleton {
+    private static class singletonInstanceHolder {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+    public static Singleton getInstance() {
+        return singletonInstanceHolder.INSTANCE;
+    }
+}
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Singleton a = Singleton.getInstance();
+        Singleton b = Singleton.getInstance();
+
+        System.out.println(a.hashCode());
+        System.out.println(b.hashCode());
+
+        if (a == b) {
+            System.out.println(true);
+        }
+    }
+}
